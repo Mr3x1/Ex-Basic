0183908f12036a12f24a86b1b1b23a012c5e0ebe
Mr. Ex <103701389+Mr3x1@users.noreply.github.com>
2022-04-23 19:11:16 +0600

    Add files via upload

---
+++ ex_basic.sh
@@ -0,0 +1,80 @@
+#!bin/bash
+
+red='\e[1;31m'
+green='\e[1;32m'
+blue='\e[1;34m'
+purple='\e[1;35m'
+cyan='\e[1;36m'
+white='\e[1;37m'
+yellow='\e[1;33m'
+clear
+echo
+echo
+echo
+echo
+echo -e $red "-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-"
+echo -e $green
+figlet Mr. Ex
+echo
+echo -e $red "-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-"
+echo
+echo -e $cyan "Author      	 :Mr. Ex"
+echo -e $cyan "Facebook      : https//facebook.com/mr3x1"
+echo -e $cyan "Facebook      : https://facebook.com/noyonvau14"
+echo
+echo -e $red "-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-"
+echo -e $white
+pkg update -y
+pkg upgrade -y
+pkg install python -y
+pkg install python2 -y
+pkg install python2-dev -y
+pkg install python3 -y 
+pkg install java -y 
+pkg install fish -y 
+pkg install ruby -y 
+pkg install help -y 
+pkg install git -y 
+pkg install host -y 
+pkg install php -y 
+pkg install perl -y 
+pkg install nmap -y 
+pkg install bash -y 
+pkg install clang -y 
+pkg install nano -y 
+pkg install w3m -y 
+pkg install havij -y 
+pkg install hydra -y 
+pkg install figlet -y 
+pkg install cowsay -y 
+pkg install curl -y 
+pkg install tar -y 
+pkg install zip -y 
+pkg install unzip -y 
+pkg install tor -y 
+pkg install google -y 
+pkg install sudo -y 
+pkg install wget -y 
+pkg install wireshark -y 
+pkg install wgetrc -y ;
+pkg install wcalc -y 
+pkg install bmon -y 
+pkg install vpn -y 
+pkg install unrar -y 
+pkg install toilet -y 
+pkg install proot -y 
+pkg install net-tools -y 
+pkg install golang -y 
+pkg install chroot -y 
+termux-chroot -y 
+pkg install macchanger -y 
+pkg install openssl -y 
+pkg install cmatrix -y 
+pkg install openssh -y 
+pkg install wireshark -y 
+termux-setup-storage -y 
+pkg install macchanger -y 
+apt update && apt upgrade -y
+echo -e $green
+figlet Completed
+echo -e $white
